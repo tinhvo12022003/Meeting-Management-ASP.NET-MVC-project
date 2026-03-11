499c9a546e68bec874a3357edeec3c729cc65174
Võ Ngọc Tính <94211002+tinhvo12022003@users.noreply.github.com>
2026-03-11 08:12:07 +0700

    fix mobile sidebar

--- obj/Debug/net10.0/MeetingManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/MeetingManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35296f80ae4588a1f3959f7016ea1e3753a1bb1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3140415298acc61dc12206b268f1e27c3b7b2fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
